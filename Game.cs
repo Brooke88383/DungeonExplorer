5f03970fadc2d38ef875ddf260c47861293e6fc3
Brooke88383 <brookemegan8@gmail.com>
2025-03-13 12:58:08 +0000

    Basic functions of gane

--- Game.cs
+++ Game.cs
@@ -11,16 +11,65 @@ namespace DungeonExplorer
         public Game()
         {
             // Initialize the game with one room and one player
+            Console.WriteLine("What is your name");
+            string users_name = Console.ReadLine();
+            player = new Player($"{users_name}", 100);
+            currentRoom = new Room("Start");
 
         }
         public void Start()
         {
             // Change the playing logic into true and populate the while loop
-            bool playing = false;
+
+            bool playing = true; 
             while (playing)
+            {
+                Console.WriteLine("What action would you like to do?");
+                Console.WriteLine("inventory, Move to the next room or quit the game.");
+                string NextAction = Console.ReadLine().ToLower();
+                {
+                    if (NextAction == "quit")
+                    {
+                        Console.WriteLine("Exiting... Thank you for playing.");
+                        playing = false;
+                    }
+        
+                    else if (NextAction == "inventory")
+                    {
+                        Console.WriteLine($"You have {player.InventoryContents()}");
+                    }
+
+                    else if (NextAction == "move")
+                    {
+                        Console.WriteLine("Moving to next room...");
+                        Random random = new Random();
+
+                        //randomly chooses out of the 2 rooms
+
+                        int RoomType = random.Next(1, 3);
+                        if (RoomType == 1)
+                        {
+                            EnemyRoom enemyRoom = new EnemyRoom();
+                            currentRoom = enemyRoom;
+                            Console.WriteLine(enemyRoom.GetDescription());
+                            enemyRoom.AttackPlayer(player);
+                        }
+                        else
+                        {
+                            ItemRoom itemRoom = new ItemRoom();
+                            currentRoom = itemRoom; 
+                            Console.WriteLine(itemRoom.GetDescription());
+                            player.PickUpItem("Apple");
+                        }
+                    }
+                    else Console.WriteLine("Invalid option please pick one of the three.");
+                    
+
+                }
             {
                 // Code your playing logic here
             }
         }
+    }
     }
 }
--- Room.cs
+++ Room.cs
@@ -1,12 +1,30 @@
-﻿namespace DungeonExplorer
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using DungeonExplorer;
+
+namespace DungeonExplorer
 {
     public class Room
     {
         private string description;
+        List<Room> rooms = new List<Room>();
 
         public Room(string description)
         {
             this.description = description;
+
+
+            
+        }
+        public void CreateRooms()
+        {
+        
+            rooms.Add(new ItemRoom());
+            rooms.Add(new EnemyRoom());
+
         }
 
         public string GetDescription()
@@ -14,4 +32,34 @@
             return description;
         }
     }
+
+
+    public class ItemRoom : Room
+    {
+        private List<string> inventory = new List<string>; 
+        public ItemRoom(string inventory) : base("This room has an item.");
+        {
+            inventory.Add("Apple");
+            Console.WriteLine("You have obtained an Apple.");
+        }
+    }
+
+    public class EnemyRoom : Room
+    {
+        public EnemyRoom() : base("You were attacked. You lost 20 health.")
+        {
+            Console.WriteLine(GetDescription());
+
+        }
+        
+        public void AttackPlayer(int damage)
+        {
+            int damage = 20; 
+            player.TakeDamage(damage);
+            
+        }
+
+        
+    }
+    
 }
