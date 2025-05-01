67aaa5f01fe86c6a5d9080b4989081b41d4d585f
Brooke88383 <brookemegan8@gmail.com>
2025-05-01 16:36:55 +0100

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Security;
@@ -7,61 +7,103 @@ using DungeonExplorer;
 
 namespace DungeonExplorer
 {
-    public class Room
+    public abstract class Room
     {
         private string description;
-        List<Room> rooms = new List<Room>();
 
         public Room(string description)
         {
             this.description = description;
-
-
-            
-        }
-        public void CreateRooms()
-        {
-        
-            rooms.Add(new ItemRoom());
-            rooms.Add(new EnemyRoom());
-
         }
 
-        public string GetDescription()
+        public virtual string GetDescription()
         {
             return description;
         }
+
+        
+        public abstract void Interact(Player player);
+    }
+
+
+    public static class ItemRandomiser
+    {
+        private static Random random = new Random();
+
+        public static Item GetRandomItem()
+        {
+            List<Item> possibleItems = new List<Item>
+        {
+            HealingPotion,
+            Sword
+        };
+
+            return possibleItems[random.Next(possibleItems.Count)];
+        }
+
+        public static Item HealingPotion = new Potion("Healing Potion", "Restores 50 health points", 30, 50);
+        public static Item Sword = new Weapon("Sword", "A powerful blade dealing 100 damage", 100, 100);
     }
 
 
     public class ItemRoom : Room
     {
-         
-        public ItemRoom() : base("This room has an item.")
+        private Item roomItem;
+
+        public ItemRoom() : base("")
         {
-            Console.WriteLine("You have obtained an Apple.");
+            roomItem = ItemRandomiser.GetRandomItem(); 
         }
-         public void GiveItemToPlayer(Player player)
-    {
-        player.PickUpItem("Apple"); 
-        Console.WriteLine("The Apple has been added to your inventory.");
-    }
+
+        public override void Interact(Player player)
+        {
+            Console.WriteLine(GetDescription());
+
+            if (roomItem != null)
+            {
+
+                player.PickUpItem(roomItem);
+            }
+            else
+            {
+                Console.WriteLine("Room did not generate an item.");
+            }
+        }
     }
 
     public class EnemyRoom : Room
     {
-        public EnemyRoom() : base("You were attacked. You lost 20 health.")
-        {
+        private Monster roomMonster;
 
-        }
-        
-        public void AttackPlayer(Player player)
+        public EnemyRoom(Monster monster) : base("")
         {
-            int damage = 20;
-            player.TakeDamage(damage);
-            
+            roomMonster = monster;
         }
 
+        public override void Interact(Player player)
+        {
+            Console.WriteLine(GetDescription());
+            Console.WriteLine($"A {roomMonster.Name} attacks! He has {roomMonster.Health} health remaining.");
+
+            if (player.EquippedWeapon != null) 
+            {
+                int weaponDamage = player.EquippedWeapon.Damage;
+                roomMonster.Health -= weaponDamage;
+                Console.WriteLine($"You deal {weaponDamage} damage to the {roomMonster.Name}. It has {roomMonster.Health} health remaining.");
+            }
+            else
+            {
+                Console.WriteLine("You have no weapon equipped to attack with!");
+            }
+
+            roomMonster.PerformAttack(player);
+        }
+
+
+
+
+    }
+}
         
     }
     
